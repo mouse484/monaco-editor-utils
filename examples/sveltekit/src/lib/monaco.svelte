48ffa69211e6adb3432d9ca70c7c666a4e04ac1b
mouse_484 <escmouse484@gmail.com>
2021-05-10 08:28:30 +0000

    chore(examples): add sveltekit examples

---
+++ examples/sveltekit/src/lib/monaco.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let container;
+
+  import loader from '@monaco-editor/loader';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    loader.init().then((monaco) => {
+      monaco.editor.create(container, {
+        value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+          '\n'
+        ),
+        language: 'javascript',
+      });
+    });
+  });
+</script>
+
+<div bind:this={container} class="container" />
+
+<style>
+  .container {
+    width: 800px;
+    height: 600px;
+  }
+</style>
---
+++ examples/sveltekit/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Monaco from '$lib/monaco.svelte';
+</script>
+
+<Monaco />
